34edd3fc7512433dc6385e86127507468230ecca
Alicja Olejnik <alicja@olejnik.net>
2021-05-21 22:02:02 +0200

    can delete characters, can delete carried items, auth for logged users

--- char_sheet/urls.py
+++ char_sheet/urls.py
@@ -1,13 +1,20 @@
 from django.urls import path
 from . import views
-from .views import CharactersListView, CharactersDetailView, CharactersCreateView, Character_itemsDetailView, ItemsCreateView, Character_itemsCreateView
+from .views import (CharactersListView,
+    CharactersDetailView, CharactersCreateView, Character_itemsDetailView,
+    ItemsCreateView, Character_itemsCreateView, CharactersUpdateView,
+    CharactersDeleteView, Character_itemsDeleteView)
 
 urlpatterns = [
     path('', CharactersListView.as_view(), name='char_sheet-home'),  # empty '' because homepage
     path('character/<pk>/', CharactersDetailView.as_view(), name='character-detail'),
     path('characters/new/', CharactersCreateView.as_view(), name='character-create'),
+    path('character/<pk>/update/', CharactersUpdateView.as_view(), name='character-update'),
+    path('character/<pk>/delete/', CharactersDeleteView.as_view(), name='character-delete'),
     path('character-items/<pk>', Character_itemsDetailView.as_view(), name='char_items-details'),
-    path('character_items/new/', Character_itemsCreateView.as_view(), name='character_items-create'),
+    path('character-items/<pk>', Character_itemsDetailView.as_view(), name='char_items-details'),
+    path('character_items/delete/<pk>', Character_itemsDeleteView.as_view(), name='character_items-delete'),    
+    path('character_items/new/<pk>', Character_itemsCreateView.as_view(), name='character_items-create'),
     path('items/new/', ItemsCreateView.as_view(), name='items-create'),
     path('site/',views.site, name='char_sheet-site'),
     path('about/', views.about, name='char_sheet-about'),
--- char_sheet/views.py
+++ char_sheet/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Items, Abilities, Race, Job_class, Information, Character_abilities, Enchantements, Item_enchantements
+from .models import (Items, Abilities, Race, Job_class, Information,
+    Character_abilities, Enchantements, Item_enchantements)
 from .models import Character_items as ci
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import (ListView, DetailView,
+    DeleteView, CreateView, UpdateView)
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 # Create your views here.
 def home(request):
     context = {
@@ -21,14 +24,14 @@ class CharactersDetailView(DetailView):
 class Character_itemsDetailView(DetailView):
     model = ci
 
-class ItemsCreateView(CreateView):
+class ItemsCreateView(LoginRequiredMixin, CreateView):
     model = Items
     fields = ['item_name', 'damage_points', 'special_ability', 'price']
 
     def form_valid(self, form):
         return super().form_valid(form)
 
-class CharactersCreateView(CreateView):
+class CharactersCreateView(LoginRequiredMixin, CreateView):
     model = Information
     fields = ['name', 'age', 'race', 'job_class', 'level', 'hit_points', 'magic_points', 'notes']
 
@@ -36,15 +39,62 @@ class CharactersCreateView(CreateView):
         form.instance.player = self.request.user
         return super().form_valid(form)
 
-class Character_itemsCreateView(CreateView):
-    model = ci
-    fields = ['character_id', 'item_name']
+class CharactersDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Information
+    success_url = '/profile/'
 
     def form_valid(self, form):
-        if form.instance.character_id.player == self.request.user:
-            return super().form_valid(form)
+        form.instance.player = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        information = self.get_object()
+        if self.request.user == information.player:
+            return True
         else:
-            return super().form_invalid(form)
+            return False
+
+
+class CharactersUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Information
+    fields = ['name', 'age', 'level', 'hit_points', 'magic_points', 'notes']
+
+    def form_valid(self, form):
+        form.instance.player = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        information = self.get_object()
+        if self.request.user == information.player:
+            return True
+        else:
+            return False
+
+class Character_itemsCreateView(LoginRequiredMixin, UserPassesTestMixin, CreateView):
+    model = ci
+    fields = ['item_name']
+
+    def form_valid(self, form):
+        form.instance.character_id = self.get_object().character_id
+        return super().form_valid(form)
+
+    def test_func(self):
+        obj = self.get_object()
+        if self.request.user == obj.character_id.player:
+            return True
+        else:
+            return False
+
+class Character_itemsDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = ci
+    success_url = '/profile/'
+
+    def test_func(self):
+        obj = self.get_object()
+        if self.request.user == obj.character_id.player:
+            return True
+        else:
+            return False
 
 def about(request):
     return render(request, 'char_sheet/about.html', {'title': 'about'})
